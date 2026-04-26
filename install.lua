09e8dbb005c9cbf684e1213a48912d5ac9af1f0d
Mark Ringer <mark.leon.ringer@gmail.com>
2026-04-26 15:45:41 +0200

    Clean up install.lua by removing unused code
    Removed unused variables and file saves for lib and reactor.

--- install.lua
+++ install.lua
@@ -1,11 +1,7 @@
 
-local libURL = "https://raw.githubusercontent.com/RinMar/Draconic-ComputerCraft-Program/master/lib/f.lua"
-local reactorURL = "https://raw.githubusercontent.com/RinMar/Draconic-ComputerCraft-Program/master/reactor.lua"
 local startupURL = "https://raw.githubusercontent.com/RinMar/Draconic-ComputerCraft-Program/master/startup.lua"
-local lib, startup, reactor
-local libFile, startupFile, reactorFile
-
-fs.makeDir("lib")
+local startup
+local startupFile
 
 function saveFiles(name, url)
   local conn = http.get(url)
@@ -15,6 +11,4 @@ function saveFiles(name, url)
   file.close()
   end
 
-saveFile("lib/f", libURL)
 saveFile("startup.lua", startupURL)
-saveFile("reactor.lua", reactorURL)
