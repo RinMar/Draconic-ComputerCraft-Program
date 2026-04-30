57523da0700e83d498a43657db55e10ff8a35c5e
Mark Ringer <mark.leon.ringer@gmail.com>
2026-04-30 09:36:32 +0200

    Refactor file download and update logic
    Refactored file saving logic into a reusable function to handle downloading and updating files more efficiently.

--- startup.lua
+++ startup.lua
@@ -5,78 +5,35 @@ local lib, lib2, startup
 local libFile, lib2File, startupFile
  
 fs.makeDir("lib")
- 
-lib = http.get(libURL)
-libFile = lib.readAll()
- 
-if fs.exists("lib/f") == false then
-    local file1 = fs.open("lib/f", "w")
-    file1.write(libFile)
-    file1.close()
-    print("API F downloaded")
-else
-    local file1 = fs.open("lib/f", "r")
-    local f = file1.readAll()
-    if libFile ~= f then
-        file1.close()
-        local file2 = fs.open("lib/f", "w")
-        file2.write(libFile)
-        file2.close()
-        
-        print("API F updating...")
+
+function saveFile(name, url)
+    local conn = http.get(url)
+    local content = conn.readAll()
+    if fs.exists(name) == false then
+        local file = fs.open(name, "w")
+        file.write(content)
+        file.close()
+        print(name, " downloaded")
     else
-        file1.close()
-        print("API F up to date")
+        local file1 = fs.open(name, "r")
+        local f = file1.readAll()
+        if libFile ~= f then
+            file1.close()
+            local file2 = fs.open(name, "w")
+            file2.write(libFile)
+            file2.close()
+           
+            print(name," updating...")
+        else
+            file1.close()
+            print(name, "up to date")
+        end
     end
-end
- 
-lib2 = http.get(libURL2)
-lib2File = lib2.readAll()
- 
-if fs.exists("lib/button") == false then
-    local file1 = fs.open("lib/button", "w")
-    file1.write(lib2File)
-    file1.close()
-    print("API Button downloaded")
-else
-    local file1 = fs.open("lib/button", "r")
-    local b = file1.readAll()
-    if lib2File ~= b then
-        file1.close()
-        local file2 = fs.open("lib/button", "w")
-        file2.write(lib2File)
-        file2.close()
-        
-        print("API Button updating...")
-    else
-        file1.close()
-        print("API Button up to date")
-    end
-end
- 
-startup = http.get(startupURL)
-startupFile = startup.readAll()
- 
-if fs.exists("reactor") == false then
-    local file1 = fs.open("reactor", "w")
-    file1.write(startupFile)
-    file1.close()
-    print("Reactor File downloaded")
-else
-    local file1 = fs.open("reactor", "r")
-    local reactor = file1.readAll()
-    if startupFile ~= reactor then
-        file1.close()
-        local file2 = fs.open("reactor", "w")
-        file2.write(startupFile)
-        file2.close()
-        
-        print("Reactor File updating...")
-    else
-        file1.close()
-        print("Reactor File up to date")
-    end
-end
+ end
+
+saveFile("lib/f.lua", libURL)
+saveFile("lib/button.lua", libURL2)
+saveFile("reactor.lua", startupURL)
  
 print("Finished")
 print("Starting Program")
